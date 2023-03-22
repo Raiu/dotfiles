29446baa9772997c599754b72c4669183b309ae5
Viktor Näslund <1299835+Raiu@users.noreply.github.com>
2023-03-22 23:57:48 +0100

    u

--- prime.sh
+++ prime.sh
@@ -132,6 +132,23 @@ LC_MEASUREMENT=sv_SE.utf8
 LC_IDENTIFICATION=sv_SE.UTF-8
 LC_ALL=
 EOF
+$SUDO tee '/etc/zsh/zshenv' >> /dev/null << EOF
+export LANG="en_GB.UTF-8"
+export LANGUAGE="en_GB:en"
+export LC_CTYPE="en_GB.UTF-8"
+export LC_NUMERIC="sv_SE.utf8"
+export LC_TIME="sv_SE.utf8"
+export LC_COLLATE="en_GB.UTF-8"
+export LC_MONETARY="sv_SE.utf8"
+export LC_MESSAGES="en_GB.UTF-8"
+export LC_PAPER="sv_SE.utf8"
+export LC_NAME="sv_SE.UTF-8"
+export LC_ADDRESS="sv_SE.UTF-8"
+export LC_TELEPHONE="sv_SE.UTF-8"
+export LC_MEASUREMENT="sv_SE.utf8"
+export LC_IDENTIFICATION="sv_SE.UTF-8"
+export LC_ALL=""
+EOF
 $SUDO tee '/etc/default/keyboard' > /dev/null << EOF
 XKBMODEL="pc105"
 XKBLAYOUT="se"
